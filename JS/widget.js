dd2ae1e3e70bd3fbf4d83d5afb0e686f6713c4c5
Vikramaditya Singh Thakur <singh.vikramsingh.vikram92@gmail.com>
2020-03-15 02:18:45 +0530

    refactored.

--- JS/widget.js
+++ JS/widget.js
@@ -1,71 +1,96 @@
-(function(d) {
+(function(d) 
+{
   var base = "https://lab.lepture.com/github-cards/";
 
   var i, count = 0;
 
   var metas = d.getElementsByTagName('meta');
   var client_url, client_id, client_secret, client_theme;
-  for (i = 0; i < metas.length; i++) {
+  for (i = 0; i < metas.length; i++) 
+  {
     var n = metas[i].getAttribute('name');
     var c = metas[i].getAttribute('content');
-    if (n === 'gc:url') {
+    if (n === 'gc:url') 
+    {
       client_url = c;
-    } else if (n === 'gc:base') {
+    } 
+    else if (n === 'gc:base') 
+    {
       base = c;
-    } else if (n === 'gc:client-id') {
+    } 
+    else if (n === 'gc:client-id') 
+    {
       client_id = c;
-    } else if (n === 'gc:client-secret') {
+    } 
+    else if (n === 'gc:client-secret') 
+    {
       client_secret = c;
-    } else if (n === 'gc:theme') {
+    } 
+    else if (n === 'gc:theme') 
+    {
       client_theme = c;
     }
   }
 
-  function queryclass(name) {
-    if (d.querySelectorAll) {
+  function queryclass(name) 
+  {
+    if (d.querySelectorAll) 
+    {
       return d.querySelectorAll('.' + name);
     }
     var elements = d.getElementsByTagName('div');
     var ret = [];
-    for (i = 0; i < elements.length; i++) {
-      if (~elements[i].className.split(' ').indexOf(name)) {
+    for (i = 0; i < elements.length; i++) 
+    {
+      if (~elements[i].className.split(' ').indexOf(name)) 
+      {
         ret.push(elements[i]);
       }
     }
     return ret;
   }
 
-  function querydata(element, name) {
+  function querydata(element, name) 
+  {
     return element.getAttribute('data-' + name);
   }
 
-  function heighty(iframe) {
-    if (window.addEventListener) {
-      window.addEventListener('message', function(e) {
-        if (iframe.id === e.data.sender) {
+  function heighty(iframe) 
+  {
+    if (window.addEventListener) 
+    {
+      window.addEventListener('message', function(e) 
+      {
+        if (iframe.id === e.data.sender) 
+        {
           iframe.height = e.data.height;
         }
       }, false);
     }
   }
 
-  function render(card, cardurl) {
+  function render(card, cardurl) 
+  {
     cardurl = cardurl || client_url;
-    if (!cardurl) {
+    if (!cardurl) 
+    {
       var theme = querydata(card, 'theme') || client_theme || 'default';
       cardurl = base + 'cards/' + theme + '.html';
     }
     var user = querydata(card, 'user');
     var repo = querydata(card, 'repo');
     var github = querydata(card, 'github');
-    if (github) {
+    if (github) 
+    {
       github = github.split('/');
-      if (github.length && !user) {
+      if (github.length && !user) 
+      {
         user = github[0];
         repo = repo || github[1];
       }
     }
-    if (!user) {
+    if (!user) 
+    {
       return;
     }
 
@@ -86,18 +111,22 @@
     iframe.setAttribute('allowtransparency', true);
 
     var url = cardurl + '?user=' + user + '&identity=' + identity;
-    if (repo) {
+    if (repo) 
+    {
       url += '&repo=' + repo;
     }
-    if (target) {
+    if (target) 
+    {
       url += '&target=' + target;
     }
-    if (key && secret) {
+    if (key && secret) 
+    {
       url += '&client_id=' + key + '&client_secret=' + secret;
     }
     iframe.src = url;
     iframe.width = width || Math.min(card.parentNode.clientWidth || 400, 400);
-    if (height) {
+    if (height) 
+    {
       iframe.height = height;
     }
     heighty(iframe);
@@ -106,12 +135,15 @@
   }
 
   var cards = queryclass('github-card');
-  for (i = 0; i < cards.length; i++) {
+  for (i = 0; i < cards.length; i++) 
+  {
     render(cards[i]);
   }
 
-  if (window.githubCard) {
+  if (window.githubCard) 
+  {
     window.githubCard.render = render;
   }
 
-})(document);
+})
+(document);
